1eb18be92147f28cd431f00f5af37d08bb501fb2
lebrancconvas <lebrancconvas@gmail.com>
2023-03-06 16:16:42 +0700

    ➕ feat(dep: add windi css

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,8 +1,7 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
-
-import './index.css';
 import App from './App';
+import 'virtual:windi.css';
 
 const root = document.getElementById('root');
 
